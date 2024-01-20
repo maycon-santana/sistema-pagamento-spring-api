0b499fbda177283dee137ab21872ad64befb56c2
MayconInforGames2017 <maycon_gus2009@hotmail.com>
2024-01-20 12:01:42 -0300

    Criando o projeto

---
+++ src/test/java/br/com/mayconinforgames/sistemapagamento/SistemaPagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mayconinforgames.sistemapagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaPagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
